5f1d09981dd08b2b4e215d319dd3d7eb5503edbb
qyl <448979688@qq.com>
2016-04-27 00:44:23 +0800

    更新

--- QYLabelDemo/QYLabelDemo/ViewController.m
+++ QYLabelDemo/QYLabelDemo/ViewController.m
@@ -23,44 +23,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //闭包回调打印
+    //闭包回调打印 
     self.myLabel.userStringTapHandler = ^(UILabel *label,NSString *string,NSRange range){
         NSLog(@"string = %@,range.location = %lu,range.length = %lu",string,range.location,range.length);
     };
 
 }
 
-
+//匹配话题
 - (IBAction)matchTopic:(id)sender {
     self.myLabel.showTopic = YES;
 }
-
+//取消匹配话题
 - (IBAction)cancelTopic:(id)sender {
     self.myLabel.showTopic = NO;
 }
-
+//取消匹配用户
 - (IBAction)cancelUser:(id)sender {
     self.myLabel.showUser = NO;
 }
-
+//匹配用户
 - (IBAction)matchUser:(id)sender {
     self.myLabel.showUser = YES;
 }
-
+//取消匹配链接
 - (IBAction)cancelLink:(id)sender {
     self.myLabel.showLink = NO;
 }
-
+//匹配链接
 - (IBAction)matchLink:(id)sender {
     self.myLabel.showLink = YES;
 }
-
+//设置随机颜色
 - (IBAction)matchTextColor:(id)sender {
     self.myLabel.matchTextColor = QYRandomColor;
 }
 
 
-
+//添加自定义字符匹配
 - (IBAction)addMyString:(id)sender {
     
     if (self.myTextFidle.text.length > 0 && ![self.myTextFidle.text isEqual: @""]) {
